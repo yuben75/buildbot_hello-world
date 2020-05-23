9f80085d9498f2c4b1757dae5f9c179f33c10451
yuben75 <yuben75.huang@google.com>
2020-05-23 08:31:00 +0800

    modified:   test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-<<<<<<< HEAD
 echo "====test.sh====="
-=======
-echo "====gitlab test.sh============================================="
->>>>>>> gitlab/master
 DIR_TOP=$(cd `dirname $0` ; pwd)
 echo "DIR_TOP="${DIR_TOP}
